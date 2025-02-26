58b68bd075a5c5b522b516b03941d9b7d66da177
Filipi Arruda <filipi.arruda@digi.ag>
2025-02-26 17:29:47 -0300

    feat: add all project files

---
+++ ___index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const server = express();
+
+server.get('/hello', (req, res) => {
+
+    const { nome, idade } = req.query;
+
+    return res.json({ 
+        title: 'Hello World',
+        message: `Olá ${nome} tudo bem?`,
+        idade: idade
+    });
+});
+
+server.get('/hello/:nome', (req, res) => {
+
+    const nome = req.params.nome;
+
+    return res.json({ 
+        title: 'Hello World',
+        message: `Olá ${nome} tudo bem?`
+    });
+
+});
+
+
+server.listen(3030);
---
+++ src/app/controllers/CustomersController.js
@@ -0,0 +1,62 @@
+class CustomersController {
+  constructor() {
+    this.customers = [
+      { id: 1, name: "Dev", site: "https://dev.to" },
+      { id: 2, name: "Google", site: "https://google.com" },
+      { id: 3, name: "Uol", site: "https://uol.com.br" },
+    ];
+  }
+
+  index(req, res) {
+    res.json(customers);
+  }
+
+  show(req, res) {
+    const id = parseInt(req.params.id);
+    const customer = customers.find((item) => item.id === id);
+    const status = customer ? 200 : 404;
+
+    console.debug("GET :: /customers/:id", customer);
+
+    res.status(status).json(customer);
+  }
+
+  create(req, res) {
+    const { name, site } = req.body;
+    const id = customers[customers.length - 1].id + 1;
+    const newCustomer = { id, name, site };
+
+    customers.push(newCustomer);
+
+    return res.status(201).json(newCustomer);
+  }
+
+  update(req, res) {
+    const id = parseInt(req.params.id);
+    const { name, site } = req.body;
+
+    const index = customers.findIndex((item) => item.id === id);
+    const status = index >= 0 ? 200 : 404;
+
+    if (index >= 0) {
+      customers[index] = { id, name, site };
+    }
+
+    return res.status(status).json(customers[index]);
+  }
+
+  destroy(req, res) {
+    const id = parseInt(req.params.id);
+
+    const index = customers.findIndex((item) => item.id === id);
+    const status = index >= 0 ? 200 : 404;
+
+    if (index >= 0) {
+      customers.splice(index, 1);
+    }
+
+    return res.status(status).json();
+  }
+}
+
+module.exports = new CustomersController();
