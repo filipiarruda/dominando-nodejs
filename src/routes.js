66282edec64b63350552e7f8052623be3153147c
Filipi Arruda <filipi.arruda@digi.ag>
2025-02-27 04:44:15 -0300

    ajustes eslint

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import customers from'./app/controllers/CustomersController';
+import customers from './app/controllers/CustomersController';
 
 const routes = new Router();
 
